9e7654f8b9f587c3fe37a59853e6f21de9e9c91c
SamuelRBatista <samuel.r.batista1983@gmail.com>
2025-03-21 10:25:34 -0300

    Initial commit

--- obj/Debug/net8.0/EcomAdmin.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcomAdmin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcomAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a805d32ea3c5a4dad8b298ce8dba6eb228b1e346")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcomAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcomAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
